ffad14af1ae110ef192082735f7a0356f7c69ff8
lucas.caldeira <lucas.caldeira@ntendencia.com.br>
2022-03-31 09:52:30 -0300

    Consultando o estoque

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,11 +1,40 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import { addReserveSuccess } from './actions';
+import { select, call, put, all, takeLatest } from 'redux-saga/effects';
+import { addReserveSuccess, updateAmountReserve } from './actions';
 import api from '../../../services/api';
 
 function* addToReserve({ id }){
-  const response = yield call(api.get, `trips/${id}`);
+  const tripExists = yield select(
+    state => state.reserve.find(trip => trip.id === id)
+  );
 
-  yield put(addReserveSuccess(response.data))
+  const myStock = yield call(api.get, `/stock/${id}`);
+  
+  const stockAmount = myStock.data.amount;
+
+  const currentStock = tripExists ? tripExists.amount : 0;
+
+  const amount = currentStock + 1;
+
+  if(amount > stockAmount){
+    alert('Quantidade máxima atigida.');
+    return;
+  }
+
+  if(tripExists){
+
+    yield put(updateAmountReserve(id, amount));
+
+  }else {
+    const response = yield call(api.get, `trips/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+    }
+  
+    yield put(addReserveSuccess(data))
+  }
+
 }
 
 export default all([
